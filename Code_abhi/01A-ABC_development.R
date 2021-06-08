d546f76f26c96e9e1b6b2434d4cec2b8463a688e
Abhishek Varghese <35830543+Sheksta@users.noreply.github.com>
2021-06-09 02:33:45 +1000

    Update 01A-ABC_development.R
    attempting to make easyABC work with multivariate prior distributions.

--- Code_abhi/01A-ABC_development.R
+++ Code_abhi/01A-ABC_development.R
@@ -67,8 +67,9 @@ ABC_Results$param %>%
 #Now test with " Dummy " observed data for the ABM. First with constraints on parameters
 
 
-
+##############################################################
 # Dummy Data Testing
+##############################################################
 
 #Declare model
 wrapper_run_model <- function(x){
@@ -94,6 +95,7 @@ wrapper_run_model <- function(x){
   
   return(sim_summary)
 }
+
 x_true <- c(1551871191,0.237,0.233,0.066,0.464,0.751)
 sum_stat_obs <- wrapper_run_model(x_true)
 
@@ -101,6 +103,8 @@ sum_stat_obs <- wrapper_run_model(x_true)
 plot(1:length(sum_stat_obs), sum_stat_obs)
 
 #Set priors
+rdirichlet(1, c(1, 1, 1, 1))
+
 dummy_prior <- list(list(c("rdirichlet",1, c(1, 1, 1, 1)), c("ddirichlet", c(1, 1, 1, 1))), 
                     c("unif",0,1), c("unif",0,1),c("unif",0,1),c("unif",0,1))
 
@@ -115,8 +119,7 @@ ABC_Results <- ABC_sequential(method="Drovandi", model=wrapper_run_model,
                               verbose = T, 
                               n_cluster = 10, 
                               use_seed = T,
-                              progress_bar = T, 
-                              prior_test="(X1 + X2 + X3 + X4) == 1")
+                              progress_bar = T)
 
 
  #Plot results
@@ -127,30 +130,161 @@ ABC_Results$param %>%
   geom_density(aes(x = V1))
 
 
+##############################################################
+# The following code tries to fix compatibility issues with enabling a dirichlet
+# multivariate distribution (or any multivariate distribution) on easyABC.
+##############################################################
+
+
+#UNCHANGED!!!
+.wrap_constants_in_model = function(prior, model, use_seed) {
+  nb_parameters = length(prior)
+  # detecting constants defined as c('unif',value,value)
+  constants_mask = array(FALSE, nb_parameters)
+  constants_values = list()
+  for (i in 1:nb_parameters) {
+    if ((prior[[i]][1] == "unif") && (as.numeric(prior[[i]][2]) == as.numeric(prior[[i]][3]))) {
+      constants_mask[i] = TRUE
+      constants_values = append(constants_values, as.numeric(prior[[i]][2]))
+    }
+  }
+  constants_values = unlist(constants_values)
+  new_prior = prior[!constants_mask]
+  if (use_seed) {
+    constants_mask = c(FALSE, constants_mask)
+    nb_parameters = nb_parameters + 1
+  }
+  old_model = model
+  # returning the prior without constants, and a new function that wraps the model
+  # with the constants
+  list(new_prior = new_prior, new_model = function(parameters) {
+    param_with_constants = array(0, nb_parameters)
+    param_with_constants[constants_mask] = constants_values
+    param_with_constants[!constants_mask] = parameters
+    old_model(param_with_constants)
+  })
+}
+
+
+#UNCHANGED!!!
+.is_included <- function(res, prior) {
+  test = TRUE
+  for (i in 1:length(prior)) {
+    test = test && (prior[[i]]$density(res[i]) > 0)
+  }
+  test
+}
+
+#UNCHANGED!!!
+.process_prior = function(prior) {
+  if (!is.list(prior)) 
+    stop("'prior' has to be a list")
+  l = length(prior)
+  new_prior = list()
+  for (i in 1:l) {
+    if (is.list(prior[[i]][1])) {
+      if (length(prior[[i]]) != 2) {
+        stop(paste("Incorrect prior specification for '", prior[[i]], "'. Please refer to the documentation.", 
+                   sep = ""))
+      }
+      new_prior[[i]] = .create_dynamic_prior(prior[[i]][[1]][1], prior[[i]][[1]][-1], 
+                                             prior[[i]][[2]][1], prior[[i]][[2]][-1])
+    } else {
+      if (any(prior[[i]][1] == c("unif", "normal", "lognormal", "exponential"))) {
+        if (prior[[i]][1] == "exponential") {
+          if (length(prior[[i]]) != 2) {
+            stop(paste("Incomplete prior information for parameter ", i, 
+                       sep = ""))
+          }
+        } else {
+          if (length(prior[[i]]) != 3) {
+            stop(paste("Incomplete prior information for parameter ", i, 
+                       sep = ""))
+          }
+        }
+        new_prior[[i]] = .create_legacy_prior(prior[[i]][1], prior[[i]][-1])
+      } else {
+        stop(paste("Only the methods 'unif', 'normal, 'lognormal' and 'exponential' are wrapped in EasyABC, unknown function '", 
+                   prior[[i]][1], "'. Please refer to the documention for specifying your prior.", 
+                   sep = ""))
+      }
+    }
+  }
+  new_prior
+}
+
+
+#DONE!!!
+.create_dynamic_prior = function(sampleName, sampleArgs, densityName, densityArgs, 
+                                 isUniform = FALSE) {
+  # $sampling: sample function with the given arguments. The used function (given
+  # by 'name' argument) takes the arguments 'args' for generating a sample.
+  # $density: create the density function with the given arguments. The used
+  # function (given by 'name' argument) takes as arguments the quantile value and
+  # the given 'args' for computing the density.  $isUniform: indicates if this
+  # prior follows an uniform distribution (condition for methods that need LHS)
+  if(grepl("dirichlet", sampleName, fixed = TRUE)){
+    list(sampling = function() {
+      do.call(sampleName, list(as.numeric(sampleArgs[1]),as.vector(as.numeric(sampleArgs[2:length(sampleArgs)]))))
+    }, density = function(value) {
+      do.call(densityName, list(t(as.matrix(value)),as.vector(as.numeric(sampleArgs[1:length(densityArgs)]))))
+    }, isUniform = isUniform || sampleName == "runif", sampleArgs = sampleArgs)
+  }else{
+    list(sampling = function() {
+      do.call(sampleName, as.list(as.numeric(sampleArgs)))
+    }, density = function(value) {
+      do.call(densityName, as.list(as.numeric(c(value, densityArgs))))
+    }, isUniform = isUniform || sampleName == "runif", sampleArgs = sampleArgs)
+  }
+}
+
+#UNCHANGED!!!
+.create_legacy_prior = function(name, args) {
+  name = name
+  args = args
+  switch(
+    EXPR = name,
+    unif = .create_dynamic_prior("runif", c(1, args), "dunif", args, TRUE),
+    normal = .create_dynamic_prior("rnorm", c(1, args), "dnorm", args, TRUE),
+    lognormal = .create_dynamic_prior("rlnorm", c(1, args), "dlnorm",
+                                      args, TRUE),
+    exponential = .create_dynamic_prior("rexp", c(1, args), "dexp",
+                                        args, TRUE)
+  )
+}
+#UNCHANGED!!!
+.is_in_parameter_constraints <- function(parameter, test) {
+  is.null(test) || eval(parse(text = gsub("[xX]([0-9]+)", "parameter[\\1]", test)))
+}
+
+#DONE!!! 
+#TODO: Density function to be changed as well.
+.sample_prior <- function(prior, test) {
+  test = NULL #Already defined somewhere I think
+  l = length(hello)
+  param = c()
+  test_passed = FALSE
+  while (!test_passed) {
+    for (i in 1:l) {
+      param = c(param, prior[[i]]$sampling())
+    }
+    test_passed = .is_in_parameter_constraints(param, test)
+  }
+  param
+}
 
 
 
+dummy_prior <- list(list(c("rdirichlet",1, c(1, 1, 1, 1)), c("ddirichlet", c(1, 1, 1, 1))), 
+                    c("unif",0,1))
+hello <- .process_prior(dummy_prior)
+.sample_prior(hello)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Priors - just uniform for now. - Update to 
-thresh_prior=list(c("unif",0,1))
-
-
-
-
+##############################################################
+#Can only proceed when the ABC functions are compatible with the dirichlet
+#multivariate distribution priors
+##############################################################
 
 obs_summary <- repr_paa(deployment$real_cap, q = 10, func = mean)
 
